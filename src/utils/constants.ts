46ca639fa08acfec2c3d18906eec9347f384e2f5
Dipika-Sonkusre <dipika@weetechsolution.com>
2025-09-11 10:10:10 +0530

    feat: Table Sorting

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const usersColumns = [
-  "ID",
-  "Name",
-  "Email",
-  "Age",
-  "Address",
-  "Phone",
-  "Actions",
+  { label: "ID", key: "id" },
+  { label: "Name", key: "name" },
+  { label: "Email", key: "email" },
+  { label: "Age", key: "age" },
+  { label: "Address", key: "address" },
+  { label: "Phone", key: "phone" },
+  { label: "Actions", key: null },
 ];
